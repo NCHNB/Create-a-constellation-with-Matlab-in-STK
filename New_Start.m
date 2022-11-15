b2fa69dfb966dfe7d331d8917824a5e41ffcf94b
NCHNB <37794775+NCHNB@users.noreply.github.com>
2022-11-15 16:32:08 +0800

    Add files via upload
    创建新项目后，设定动画时间回到起始点

--- New_Start.m
+++ New_Start.m
@@ -8,6 +8,7 @@ root.NewScenario(Name);
 root.CurrentScenario.SetTimePeriod(StartTime, EndTime);
 root.CurrentScenario.Epoch = StartTime;
 root.UnitPreferences.Item('DateFormat').SetCurrentUnit('EpMin');%时间格式转分钟格式
+root.ExecuteCommand('Animate * Reset');% 设定动画时间回到起始点，这里用的是Connect命令
 sc = root.CurrentScenario;
 end
 
